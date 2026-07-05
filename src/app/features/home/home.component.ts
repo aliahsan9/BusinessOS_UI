a49d7027524e3907420b3ea30952aed6e00a681e
Ali Ahsan <aliahsan8751@gmail.com>
2026-07-05 18:42:49 +0500

    fix copilot

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { TestimonialsComponent } from '../testimonials/testimonials.component';
 import { FooterComponent } from '../footer/footer.component';
-import { AboutComponent } from "../about/about.component";
-import { ServicesComponent } from '../services/services.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, NavbarComponent, TestimonialsComponent, FooterComponent, AboutComponent, ServicesComponent],
+  imports: [RouterModule, NavbarComponent, TestimonialsComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
